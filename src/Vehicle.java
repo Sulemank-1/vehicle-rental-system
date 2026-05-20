4be563b02619e5bb2f7d39960d0d799013d52a57
Sulemank-1 <suleman.dev42@gmail.com>
2026-05-20 08:13:25 +0500

    Add Vehicle class
    This class represents a vehicle

---
+++ src/Vehicle.java
@@ -0,0 +1,63 @@
+public class Vehicle {
+    //Data Fields
+    private String licensePlate;
+    private String model;
+    private double dailyRate;
+    private boolean isRented;
+
+    //Constructor
+    public Vehicle(String licensePlate, String model, double dailyRate, boolean isRented){
+        this.licensePlate = licensePlate;
+        this.model = model;
+        this.dailyRate = dailyRate;
+        this.isRented = isRented;
+    }
+
+    //Getters
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getDailyRate() {
+        return dailyRate;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+
+    //Setters
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setDailyRate(double dailyRate) {
+        this.dailyRate = dailyRate;
+    }
+
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+
+    //Methods
+    public void rentVehicle(){
+        isRented = true;
+    }
+
+    public void returnVehicle(){
+        isRented = false;
+    }
+
+    @Override
+    public String toString(){
+        return "Licence Plate: " + getLicensePlate() + "| Model: " + getModel() + "| Daily Rate: " + getDailyRate() + "| Rented Status " + ((isRented()) ? "Rented" : "Not Rented");
+    }
+}
